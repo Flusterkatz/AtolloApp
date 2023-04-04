a0bce0f51c4f1be041a08eb322fbc84f6b7b464d
Flusterkatz <flusterkatz@gmail.com>
2023-04-04 16:35:15 -0400

    - created splash screen design

--- lib/SplashScreen.dart
+++ lib/SplashScreen.dart
@@ -3,15 +3,6 @@ import 'package:flutter/material.dart';
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -21,66 +12,69 @@ class SplashScreen extends StatefulWidget {
 class _MyHomePageState extends State<SplashScreen> {
   int _counter = 0;
 
-  void _incrementCounter() {
+  /*void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
-  }
+  }*/
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      backgroundColor: const Color(0xff003D53),
+      body: Column(
+        children: <Widget>[
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: 150,
+            child: FittedBox(
+                fit: BoxFit.fill,
+                child: Image.asset('assets/images/splash_top.png')
+            )
+          ),
+          Expanded(
+            child: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    height: 150,
+                    child: Image.asset('assets/images/shield_noText.png')
+                  ),
+                  const SizedBox(height: 50),
+                  FilledButton(
+                    style: FilledButton.styleFrom(
+                        backgroundColor: const Color(0x50ffffff),
+                        padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 50.0),
+                    ),
+                    onPressed: () {},
+                    child: const Text(
+                        'LOG IN',
+                        style: TextStyle(fontSize: 30)
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'SIGN UP',
+                      style: TextStyle(fontSize: 30, decoration: TextDecoration.underline, color: const Color(0xffCADC48))
+                    )
+                  )
+                ]
+              )
+            )
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: 150,
+            child: FittedBox(
+              fit: BoxFit.fill,
+              child: Image.asset('assets/images/splash_bottom.png')
+            )
+          )
+        ]
+      )
     );
   }
 }
